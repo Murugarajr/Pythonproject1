980e95816742542e65c26e75aab75b655f260840
unknown <murugarajr@gmail.com>
2019-02-12 00:47:00 +0530

    first selenium file check-in

---
+++ Selenium_Test1.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome("C:\\Users\\Admin\\PycharmProjects\\Seleniumtest\\Driver\\chromedriver.exe")
+
+driver.set_page_load_timeout(10)
+driver.get("http://google.co.in")
+driver.find_element_by_name("q").send_keys("nishaptham")
+# driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+
+time.sleep(4)
+driver.maximize_window()
+print("Tested sucessfully!")
+driver.quit()
